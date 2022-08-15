cf59dd16d5c03af31a444f5aabe11f5205ea6b05
William Poulin <williampoulin01@gmail.com>
2022-08-14 23:06:57 -0400

    chore(meshswap): refactor meshswap supply token fetcher using template

--- src/apps/meshswap/polygon/meshswap.supply.token-fetcher.ts
+++ src/apps/meshswap/polygon/meshswap.supply.token-fetcher.ts
@@ -4,13 +4,15 @@ import _ from 'lodash';
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { buildDollarDisplayItem } from '~app-toolkit/helpers/presentation/display-item.present';
-import { getTokenImg } from '~app-toolkit/helpers/presentation/image.present';
-import { ContractType } from '~position/contract.interface';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
+import { DefaultDataProps, StatsItem } from '~position/display.interface';
+import {
+  AppTokenTemplatePositionFetcher,
+  DisplayPropsStageParams,
+  UnderlyingTokensStageParams,
+} from '~position/template/app-token.template.position-fetcher';
 import { Network } from '~types/network.interface';
 
-import { MeshswapContractFactory } from '../contracts';
+import { MeshswapContractFactory, MeshswapSinglePool } from '../contracts';
 import { MESHSWAP_DEFINITION } from '../meshswap.definition';
 
 const appId = MESHSWAP_DEFINITION.id;
@@ -18,17 +20,25 @@ const groupId = MESHSWAP_DEFINITION.groups.supply.id;
 const network = Network.POLYGON_MAINNET;
 
 @Register.TokenPositionFetcher({ appId, groupId, network })
-export class PolygonMeshswapSupplyTokenFetcher implements PositionFetcher<AppTokenPosition> {
+export class PolygonMeshswapSupplyTokenFetcher extends AppTokenTemplatePositionFetcher<MeshswapSinglePool> {
+  appId = appId;
+  groupId = groupId;
+  network = network;
+
   constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(MeshswapContractFactory) private readonly meshswapContractFactory: MeshswapContractFactory,
-  ) {}
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(MeshswapContractFactory) private readonly contractFactory: MeshswapContractFactory,
+  ) {
+    super(appToolkit);
+  }
 
-  async getPositions() {
+  getContract(address: string): MeshswapSinglePool {
+    return this.contractFactory.meshswapSinglePool({ network, address });
+  }
+
+  async getAddresses(): Promise<string[]> {
     const multicall = this.appToolkit.getMulticall(network);
-    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
-
-    const singlePoolFactoryContract = this.meshswapContractFactory.meshswapSinglePoolFactory({
+    const singlePoolFactoryContract = this.contractFactory.meshswapSinglePoolFactory({
       network,
       address: '0x504722a6eabb3d1573bada9abd585ae177d52e7a',
     });
@@ -36,81 +46,31 @@ export class PolygonMeshswapSupplyTokenFetcher implements PositionFetcher<AppTok
     const poolCountRaw = await multicall.wrap(singlePoolFactoryContract).getPoolCount();
     const poolCount = Number(poolCountRaw);
 
-    const positions = await Promise.all(
+    const supplyAddresses = await Promise.all(
       _.range(0, poolCount).map(async index => {
         const addressRaw = await multicall.wrap(singlePoolFactoryContract).getPoolAddressByIndex(index);
-        const address = addressRaw.toLowerCase();
-        const singlePoolContract = this.meshswapContractFactory.meshswapSinglePool({
-          network,
-          address,
-        });
-
-        const [
-          totalSupplyRaw,
-          decimals,
-          underlyingTokenAddressRaw,
-          symbol,
-          label,
-          cashRaw,
-          borrowAmountRaw,
-          exchangeRateRaw,
-        ] = await Promise.all([
-          multicall.wrap(singlePoolContract).totalSupply(),
-          multicall.wrap(singlePoolContract).decimals(),
-          multicall.wrap(singlePoolContract).token(),
-          multicall.wrap(singlePoolContract).symbol(),
-          multicall.wrap(singlePoolContract).name(),
-          multicall.wrap(singlePoolContract).getCash(),
-          multicall.wrap(singlePoolContract).totalBorrows(),
-          multicall.wrap(singlePoolContract).exchangeRateStored(),
-        ]);
-
-        const underlyingTokenAddress = underlyingTokenAddressRaw.toLowerCase();
-
-        const tokens = baseTokens.find(x => x.address == underlyingTokenAddress);
-        if (!tokens) return null;
-
-        const exchangeRate = Number(exchangeRateRaw) / 10 ** 18;
-        const cash = Number(cashRaw) / 10 ** decimals;
-        const borrowAmount = Number(borrowAmountRaw) / 10 ** decimals;
-
-        const liquidity = borrowAmount + cash;
-        const supply = Number(totalSupplyRaw) / 10 ** decimals;
-        const pricePerShare = 1;
-        const price = tokens.price;
-
-        const statsItems = [{ label: 'Liquidity', value: buildDollarDisplayItem(liquidity) }];
-
-        const poolToken: AppTokenPosition = {
-          type: ContractType.APP_TOKEN,
-          appId,
-          groupId,
-          address,
-          network,
-          supply,
-          decimals,
-          symbol,
-          price,
-          pricePerShare,
-          tokens: [tokens],
-
-          dataProps: {
-            liquidity,
-            exchangeRate,
-          },
-
-          displayProps: {
-            label,
-            secondaryLabel: symbol,
-            images: [getTokenImg(tokens.address, network)],
-            statsItems,
-          },
-        };
-
-        return poolToken;
+        return addressRaw.toLowerCase();
       }),
     );
+    return supplyAddresses;
+  }
 
-    return _.compact(positions);
+  async getUnderlyingTokenAddresses({ contract }: UnderlyingTokensStageParams<MeshswapSinglePool>): Promise<string> {
+    return await contract.token();
+  }
+
+  async getStatsItems({
+    appToken,
+    contract,
+  }: DisplayPropsStageParams<MeshswapSinglePool, DefaultDataProps>): Promise<StatsItem[]> {
+    const cashRaw = await contract.getCash();
+    const borrowAmountRaw = await contract.totalBorrows();
+    const decimals = appToken.decimals;
+
+    const cash = Number(cashRaw) / 10 ** decimals;
+    const borrowAmount = Number(borrowAmountRaw) / 10 ** decimals;
+    const liquidity = cash + borrowAmount;
+
+    return [{ label: 'Liquidity', value: buildDollarDisplayItem(liquidity) }];
   }
 }
